52f44d94605bb2234c2581b4b26b08dee5624e19
Olga Myazina <olga.myazina@balance-pl.ru>
2021-11-26 13:09:04 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+  entry: './dist/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    globalObject: 'this',
+    library: {
+      name: 'jsLibTest',
+      type: 'umd',
+    },
+  },
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'React',
+      root: 'React',
+    },
+    'prop-types': {
+      commonjs: 'prop-types',
+      commonjs2: 'prop-types',
+      amd: 'PropTypes',
+      root: 'PropTypes',
+    },
+  },
+}
